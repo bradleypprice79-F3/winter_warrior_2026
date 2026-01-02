530f71f8f1c7098f1d60edeea80f06a315c0b6f4
bradleypprice79-F3 <bradley.p.price79@gmail.com>
2026-01-02 13:46:05 +0000

    loaded current data

--- data/reports/latest_files.js
+++ data/reports/latest_files.js
@@ -1,8 +1,8 @@
 const latestFiles = {
-  individual: "individual_scores_20260101_0907.csv",
-  team: "team_scores_20260101_0907.csv",
-  lone_pax: "lone_pax_report_20260101_0907.csv",
-  aggregated: "checklist_table_20260101_0907.csv",
-  individualstandings: "individualstandings_20260101_0907.csv",
-  current_timestamp: "2026-01-01 09:07"
+  individual: "individual_scores_20260102_0742.csv",
+  team: "team_scores_20260102_0742.csv",
+  lone_pax: "lone_pax_report_20260102_0742.csv",
+  aggregated: "checklist_table_20260102_0742.csv",
+  individualstandings: "individualstandings_20260102_0742.csv",
+  current_timestamp: "2026-01-02 07:42"
 };
